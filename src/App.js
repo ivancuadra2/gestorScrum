3cb358a140fc73bac3e6b80e7c50e163cae23678
Ivan <cuadradoivan1990@gmail.com>
2020-02-09 03:36:11 -0300

    Sin API Key

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import Button from '@material-ui/core/Button';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB28SiRXzXt4PdeepGdgUym7XWh5KYWstw",
-  authDomain: "gestorscrum.firebaseapp.com",
-  databaseURL: "https://gestorscrum.firebaseio.com",
-  projectId: "gestorscrum",
-  storageBucket: "gestorscrum.appspot.com",
-  messagingSenderId: "142439088048",
-  appId: "1:142439088048:web:d15e88a0e90dddaf872075",
-  measurementId: "G-PHVK5BHRMD"
+
 };
 
 firebase.initializeApp(firebaseConfig)
