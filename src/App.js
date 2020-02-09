e5e5368b38fe6cf310595114d67e8873941f62d8
Ivan <cuadradoivan1990@gmail.com>
2020-02-08 22:57:41 -0300

    gestorScrum en firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import Button from '@material-ui/core/Button';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB28SiRXzXt4PdeepGdgUym7XWh5KYWstw",
+  authDomain: "gestorscrum.firebaseapp.com",
+  databaseURL: "https://gestorscrum.firebaseio.com",
+  projectId: "gestorscrum",
+  storageBucket: "gestorscrum.appspot.com",
+  messagingSenderId: "142439088048",
+  appId: "1:142439088048:web:d15e88a0e90dddaf872075",
+  measurementId: "G-PHVK5BHRMD"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" color="primary">
+      gestorscrum!
+  </Button>
   );
 }
 
